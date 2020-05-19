ab3f7fdf564cd632f7e58a96e5549b01f0d386c3
momo <192552396@qq.com>
2020-05-19 17:12:19 +0800

    新增了获取bodyBytes以及Document的方法, 以及将gbk编码的网页直接转化为utf-8编码的方法

--- lib/http_request.dart
+++ lib/http_request.dart
@@ -1,8 +1,13 @@
 library http_request;
 
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
+import 'package:gbk2utf8/gbk2utf8.dart';
+import 'package:html/dom.dart';
+import 'package:html/parser.dart';
+import 'package:http/http.dart' as http;
 
 // dio的请求简单封装类
 class HttpRequest {
@@ -58,5 +63,66 @@ class HttpRequest {
             '/storage/emulated/0${savePath.startsWith('/') ? savePath : '/$savePath'}',
             headers: headers,
             queryParameters: queryParameters);
+    }
+
+    static Future<Uint8List> getBodyBytes(String url, { Map<String, dynamic> headers }) async {
+        return http.get(url, headers: headers).then((res) {
+            return res.bodyBytes;
+        });
+    }
+
+    static Future<Uint8List> postBodyBytes(String url,
+        {FormData data, Map<String, dynamic> headers }) async {
+        return http.post(url, headers: headers, body: data).then((res) {
+            return res.bodyBytes;
+        });
+    }
+
+    /// 从gbk的网页, 返回utf-8的内容
+    static Future<String> getFromGbkHtml(String url, { Map<String, dynamic> headers }) async {
+        return getBodyBytes(url, headers: headers).then((bodyBytes) {
+            return gbk.decode(bodyBytes);
+        });
+    }
+
+    /// 从gbk的网页, 返回utf-8的内容
+    static Future<String> postFromGbkHtml(String url,
+        {FormData data, Map<String, dynamic> headers }) async {
+        return getBodyBytes(url, headers: headers).then((bodyBytes) {
+            return gbk.decode(bodyBytes);
+        });
+    }
+
+    /// 返回html文档对象
+    static Future<Document> getDocument(String url,
+        {Map<String, dynamic> headers, Map<String, dynamic> queryParameters}) async {
+        return get(url, headers: headers, queryParameters: queryParameters).then((html) {
+            return parse(html);
+        });
+    }
+
+    /// 返回html文档对象
+    static Future<Document> postDocument(String url,
+        {FormData data, Map<String, dynamic> headers, Map<String, dynamic> queryParameters}) async {
+        return post(url, data: data, headers: headers, queryParameters: queryParameters).then((
+            html) {
+            return parse(html);
+        });
+    }
+
+    /// 返回html文档对象
+    static Future<Document> getDocumentFromGbkHtml(String url,
+        {Map<String, dynamic> headers}) async {
+        return getFromGbkHtml(url, headers: headers).then((html) {
+            return parse(html);
+        });
+    }
+
+    /// 返回html文档对象
+    static Future<Document> postDocumentFromGbkHtml(String url,
+        {FormData data, Map<String, dynamic> headers }) async {
+        return postFromGbkHtml(url, headers: headers, data: data).then((html) {
+            return parse(html);
+        });
     }
 }
